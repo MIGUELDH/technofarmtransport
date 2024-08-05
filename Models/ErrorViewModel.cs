68cb3039a2a36af74fc1d2e966211e54b0e4b525
luis miguel dh <LUISMIGUELDARAVINA@GMAIL.COM>
2024-08-05 23:19:27 +1000

    new
    nuevo

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace technofarmtransport2.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
